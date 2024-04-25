bcef17de32ed1c0061158b9cdc5baea08d26d2ea
devaprabaharan <devaprabaharan@gmail.com>
2024-04-25 14:17:07 -0500

    container updates

--- MarketingApp/Container/src/App.js
+++ MarketingApp/Container/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MarketingApp from "./marketing/MarketingApp";
+import MarketingApp from "marketing/MarketingApp";
 
 export default () => {
     return (<div><h1>hi there</h1><hr/><MarketingApp/></div>)
