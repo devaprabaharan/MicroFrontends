772bfedeed99b7f964c1653d29407e74f4fc5c53
devaprabaharan <devaprabaharan@gmail.com>
2024-04-26 15:01:57 -0500

    added header, randomly generated classname

--- MarketingApp/Container/src/App.js
+++ MarketingApp/Container/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import MarketingApp from "./Components/MarketingApp";
+import Header from "./Components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+//unique randomly generated namespace so that css classnames will not collide with mfe's
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
-    return (<div><h1>hi there</h1><hr/><MarketingApp/></div>)
-}
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
