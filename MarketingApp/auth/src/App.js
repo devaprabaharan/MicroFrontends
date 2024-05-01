34efd21a741979107a798b2575b2a986cc1007a5
devaprabaharan <devaprabaharan@gmail.com>
2024-05-01 11:00:41 -0500

    updates to the workflow

--- MarketingApp/auth/src/App.js
+++ MarketingApp/auth/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Switch, Route, Router} from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import Signin from './components/Signin';
-import Signup from './components/Signup';
 import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
